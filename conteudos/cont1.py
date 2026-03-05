4af191b0eaef6f7e2b913e12caa78127070ecef8
mariazambao-maker <maria.zambao@escola.pr.gov.br>
2026-03-05 11:28:32 -0300

    Create cont1.py

---
+++ conteudos/cont1.py
@@ -0,0 +1,4 @@
+#int igual a numeros inteiros: 7,0, -4
+#float para numeros reais ou com virgula: 0.45, 7.6
+#bool para valores logicos verdadeiros ou falso: 
+#str e para texto ou grupos de caracteres
