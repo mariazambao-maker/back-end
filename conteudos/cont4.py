7da71df1efbb031d944d25d0bee6e2f1e59a234d
mariazambao-maker <maria.zambao@escola.pr.gov.br>
2026-03-11 11:39:23 -0300

    Create cont4.py

---
+++ conteudos/cont4.py
@@ -0,0 +1,7 @@
+#0 para + é utilizado como operador de adição
+#0 - é utilizado como sinal de subtração
+#0* é utilizado para fazer a multiplicação
+#/ é utilizado para a divisão
+#** é utilizado para fazer a exponenciação
+#// é para fazer fazer divisão inteira
+#% utilizado para o resto da divisão
