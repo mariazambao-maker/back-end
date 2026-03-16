83a89be527a5051c02b94900e5e72a53bd11f534
mariazambao-maker <maria.zambao@escola.pr.gov.br>
2026-03-16 10:50:50 -0300

    Create cont6.py

---
+++ conteudos/cont6.py
@@ -0,0 +1,16 @@
+#Podemos escrever um resultado na quantidade de caracteres específica ao colocar o valor dentro da mácara após escrever
+nome = input ('Qual é o seu nome?')
+print('prazer em conhece-lo {:20}!'.format(nome))
+Qual é o seu nome?Gustavo
+prazer em conhece-lo Gustavo
+1
+#Podemos alinhar o nome dentro do número de caracteres que desejamos
+#Alinhamento para direita
+#Alinhamento para a esquerda
+#Alinhamento no centro
+nome input ('Qual é o seu nome?')
+print('prazer em conhece-lo {:>20)!'.format(nome)
+nome input ('Qual é o seu nome?')
+print('prazer em conhece-lo (:<20)1'.format(nome))
+nome input ('Qual é o seu nome?')
+print('prazer em conhece-lo (:^20)!'.format(nome))
