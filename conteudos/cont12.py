fa5ab3296259fbbe1c44b641318b8680ef583d6a
mariazambao-maker <maria.zambao@escola.pr.gov.br>
2026-05-11 10:56:32 -0300

    Update cont12.py

--- conteudos/cont12.py
+++ conteudos/cont12.py
@@ -3,7 +3,7 @@ Estrutura sequencial: Os comandos condicionais são executados de cima para baix
 Até esse momento estávamos trabalhando nessa lógica, porém nas estruturas condicionais temos duas possibilidades de caminhos.
 Cada caminho terá um bloco de comando executados se ele for escolhido. 
 Em programação utilizamos o comando se, se alguma condição verdadeira executa um bloco de códigos, e podemos utilizar um bloco se não para executar quando a condição for falsa.
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   10:45
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
 Condicionais
 Condições
 Em condicionais devemos deixar o código indentado, ou seja, com o espaçamento correto demonstrando qual bloco de código pertece ao operador condicional. 
@@ -11,3 +11,11 @@ Para realizar a indentração podemos utilizar a tecla tab.
 Em python para o se utilizamos if+confição+:, já o senão utilizamos o else +:, depois dos dois pontos nos dois casos colocamos os blocos de códigos que serão executados. 
 Nesse tipo de condicional apenas um bloco é executado.
 11 de mai. de 2026 às 10:45.png
+
+Condições (exemplo)
+tempo = int(input("Quantos anos tem seu carro?"))
+If tempo<=3:
+print('carro novo')
+else =
+print("carro velho")
+print("--fim--")
