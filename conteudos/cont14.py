315a9e0b935321a7daa6bb0ab5cb862c124e3bac
mariazambao-maker <maria.zambao@escola.pr.gov.br>
2026-08-26 10:48:04 -0300

    Create cont14.py

---
+++ conteudos/cont14.py
@@ -0,0 +1,4 @@
+While infinito
+Nesse laço trabalhamos com um comando de parada que desvia a execução do bloco de código para o final do laço.
+Esse comando e chamado de break.
+Esse comando é executado em laços while que não possuem testes lógicos, onde escrevemos while true no loop, que sem o break seriam executados pra sempre.
