a3af2986aaad3219a8443b18a4172561e2df6b19
mariazambao-maker <maria.zambao@escola.pr.gov.br>
2026-09-14 11:40:31 -0300

    Create cont15.py

---
+++ conteudos/cont15.py
@@ -0,0 +1,10 @@
+Tuplas(variáveis compostas)
+ Relembrando do primeiro trimestre, toda vez que um variável simples é declarada um espaço na memória e criado no computador.
+Onde valores são guardados pelo operador de .
+Em variáveis simples se eu quiser guardar mais um valor dentro do espaço criado na memória o que estava armazenado anteriormente é excluido e o novo valor é armazenado.
+Para resolvermos esse problema podemos criar mais espaços na memória em uma mesma variável, e uma dessas formas e com o uso de tuplas.
+Os elementos de uma tupla pode ser selecionados através de seus índices que inicia-se no valor 0
+Tuplas(variáveis compostas) (3 tec mario)
+ Dentro das tuplas podemos usar métodos de fatiamento, de length e o uso das variáveis de repetição.
+As tuplas são imutáveis em Python ou seja os valores armazenados nessas variáveis não podem ser alterados após definidos.
+As tuplas são definidas entre parênteses
