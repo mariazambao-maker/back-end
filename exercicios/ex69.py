226b7b59e8b76986ad040d9d171d0b78f4644cdb
mariazambao-maker <maria.zambao@escola.pr.gov.br>
2026-09-21 12:27:30 -0300

    Create ex69.py

---
+++ exercicios/ex69.py
@@ -0,0 +1,12 @@
+lanche ('Hamburguer', 'Suco', 'Pizza', 'Pudim')
+print(sorted(lanche)) #ordena em ordem alfabética
+a = (2, 5, 4)
+b = (5, 8, 1, 2)
+print(a)
+print(b)
+c = a + b
+print(c)
+print(len(c))
+print(c.count(5)) #quantas vezes esta aparecendo o 5 em c
+print(c.index(8)) #mostra a posição pegando a primeira ocorrencia
+print(c.index(5, 2))# começa a apartir da posição 2
